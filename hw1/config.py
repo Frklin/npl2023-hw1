0fc250a583ddeb9f5c16b1b7485275a7d3d4b0f5
Francesco <palandrafrancesco@gmail.com>
2023-04-30 16:21:54 +0200

    minor changes on config

--- hw1/config.py
+++ hw1/config.py
@@ -1,4 +1,38 @@
-SLIDING_WINDOW  = 5          # Size of the sliding window
-NEG_SAMPLES     = 5          # Negative samples for each word
+# EMBEDDINGS
+EMBEDDING_MODEL         = "GemGlove" # "GenW2V", "GenGlove", "Fasttext"
+EMBEDDING_SIZE          = 300        # Size of the embedding vector
+WINDOW_SIZE             = 5          # Size of the sliding window
+NEGATIVE_SAMPLES        = 5          # Negative samples for each word
+UNK_TOKEN               = "<UNK>"    # Unknown token
+PAD_TOKEN               = "<PAD>"    # Padding token
+PAD_IDX                 = 0          # Padding index
+PAD_VAL                 = -100       # Padding value
 
-EMBEDDING_SIZE  = 300        # Size of the embedding vector
+# LSTM
+EPOCHS                  = 30         # Number of epochs
+HIDDEN_SIZE             = 1024       # Size of the hidden layer
+BATCH_SIZE              = 16         # Size of the batch
+N_LSTMS                 = 2          # Number of LSTM layers
+LEARNING_RATE           = 1e-3       # Learning rate
+DROPRATE                = 0.5        # Dropout rate
+CLASSIFIER              = 'softmax'  # 'softmax' or 'crf'
+OPTIMIZER               = 'adam'     # 'adam', 'nadam', sgd', 'adagrad'
+WEIGHT_DECAY            = 1e-5       # Weight decay
+
+# OS PATHS
+ROOT                    = '../../data'
+TRAIN_PATH              = ROOT + '/train.jsonl'
+VAL_PATH                = ROOT + '/dev.jsonl'
+TEST_PATH               = ROOT + '/test.jsonl'
+
+EMBEDDING_MODEL_PATH    = "/EmbModels/" + EMBEDDING_MODEL
+
+# SEED
+SEED                    = 42
+
+
+
+
+
+
+
--- hw1/stud/embeddings.py
+++ hw1/stud/embeddings.py
@@ -2,22 +2,30 @@ import numpy as np
 import config
 import torch
 from gensim.models import KeyedVectors
+import gensim.downloader as api
+import os 
 
 
 
-def load_embeddings(embedding_path: str = config.EMBEDDINGS_PATH):
-  
-    word2vec = KeyedVectors.load(embedding_path) 
+def load_embeddings(embedding_path: str = config.EMBEDDINGS_PATH, embedding_type: str = config.EMBEDDING_MODEL):
+    
+    if not os.path.exists(embedding_path):
+        print("Embedding file not found. Downloading...")
+        emb_file = api.load(embedding_type)
+        emb_file.save(embedding_path)
+        print("Embedding file saved to {}".format(embedding_path))
+    else:
+        emb_file = KeyedVectors.load(embedding_path) 
 
-    word2idx = word2vec.key_to_index
-    embeddings = word2vec.vectors
+    word2idx = emb_file.key_to_index
+    embeddings = emb_file.vectors
 
     word2idx['<PAD>'] = config.PAD_IDX
     word2idx['<UNK>'] = len(word2idx)-1
 
-    embeddings[config.PAD_IDX] = np.zeros((1, word2vec.vector_size), dtype=np.float32)
+    embeddings[config.PAD_IDX] = np.zeros((1, emb_file.vector_size), dtype=np.float32)
     # embeddings = np.append(embeddings, np.zeros((1, word2vec.vector_size), dtype=np.float32), axis=0)
-    embeddings = np.append(embeddings, np.random.rand(1, word2vec.vector_size).astype(np.float32), axis=0)
+    embeddings = np.append(embeddings, np.random.rand(1, emb_file.vector_size).astype(np.float32), axis=0)
     
     return torch.tensor(embeddings, dtype=torch.float32), word2idx
 
