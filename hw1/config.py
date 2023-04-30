cd7b19cbf294c4b21272fe50d3c062b9918ffd63
Francesco Palandra <palandra.1849712@studenti.uniroma1.it>
2023-04-30 17:23:03 +0200

    correct code

--- hw1/config.py
+++ hw1/config.py
@@ -1,5 +1,5 @@
 # EMBEDDINGS
-EMBEDDING_MODEL         = "GemGlove" # "GenW2V", "GenGlove", "Fasttext"
+EMBEDDING_MODEL         = "GenGlove" # "GenW2V", "GenGlove", "Fasttext"
 EMBEDDING_SIZE          = 300        # Size of the embedding vector
 WINDOW_SIZE             = 5          # Size of the sliding window
 NEGATIVE_SAMPLES        = 5          # Negative samples for each word
@@ -15,17 +15,17 @@ BATCH_SIZE              = 16         # Size of the batch
 N_LSTMS                 = 2          # Number of LSTM layers
 LEARNING_RATE           = 1e-3       # Learning rate
 DROPRATE                = 0.5        # Dropout rate
-CLASSIFIER              = 'softmax'  # 'softmax' or 'crf'
+CLASSIFIER              = 'crf'  # 'softmax' or 'crf'
 OPTIMIZER               = 'adam'     # 'adam', 'nadam', sgd', 'adagrad'
 WEIGHT_DECAY            = 1e-5       # Weight decay
 
 # OS PATHS
-ROOT                    = '../../data'
+ROOT                    = './data'
 TRAIN_PATH              = ROOT + '/train.jsonl'
 VAL_PATH                = ROOT + '/dev.jsonl'
 TEST_PATH               = ROOT + '/test.jsonl'
 
-EMBEDDING_MODEL_PATH    = "/EmbModels/" + EMBEDDING_MODEL
+EMBEDDINGS_PATH    = "./hw1/stud/EmbModels/" + EMBEDDING_MODEL
 
 # SEED
 SEED                    = 42
